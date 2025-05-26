7e078c8cd55cb957c0e2186a9479062c2f1bd5b5
joe <jjhowe@protonmail.ch>
2025-05-26 14:12:50 +0100

    Removed authentication for login request.

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -4,7 +4,7 @@ const { authenticateToken } = require('../middleware/authentication');
 const authController = new AuthController();
 
 auth.post('/register', authenticateToken, (req, res) => authController.register(req, res));
-auth.post('/login', authenticateToken, (req, res) => authController.login(req, res));
+auth.post('/login', (req, res) => authController.login(req, res));
 auth.get('/logout', authenticateToken, (req, res) => authController.logout(req, res));
 
 module.exports = auth;
